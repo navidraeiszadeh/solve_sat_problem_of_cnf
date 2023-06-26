3dcd6d9a1b7d37a5b6ce5a0900d0c1ae284d333c
Navid Raeiszadeh <Navidraeiszadeh@gmail.com>
2023-06-26 10:29:18 +0330

    some little changes in cross over

--- GeneticSolver.py
+++ GeneticSolver.py
@@ -5,7 +5,6 @@ import pysat as py
 import math
 from pysat.formula import CNF
 
-
 cnf = CNF('Input.cnf')
 # number of current generation
 number_of_generation = cnf.nv
@@ -18,7 +17,7 @@ class creature:
         self.gene = gene
         self.fitness = fitness(gene)     #calculate the fitness of that!
 
-def fitness(cnf_test):
+def fitness(cnf_test): #calculate the parantese count
     global cnf
     contributions = {}
     tmp = 0
@@ -29,13 +28,13 @@ def fitness(cnf_test):
             contributions[literal].add(tmp)    
         tmp += 1
     count_of_sat = 0
-    satisfied_parts = np.zeros(len(cnf.clauses))
+    tempList = np.zeros(len(cnf.clauses))
     for i in range(cnf.nv):
         if cnf_test[i] == 1:
             contribution_set = contributions[i+1]
             for i in contribution_set:
-                if satisfied_parts[i] == 0:
-                    satisfied_parts[i] = 1
+                if tempList[i] == 0:
+                    tempList[i] = 1
                     count_of_sat += 1
     return count_of_sat                    
             
@@ -54,15 +53,13 @@ def choose_parent(generation_list):
     parent_list2 = [generation_list[i] for i in range(0, bottom_gen_num)]
     return parent_list2.extend(best_parent)
             
-
-
 def cross_over(P1 , P2):
     global cnf
     random_index = random.randint( 1 , cnf.nv)
-    left_gene = [P1.gene[i] for i in range(random_index)]  # set it in a dictionory
-    right_gene = [P2.gene[i] for i in range(random_index, cnf.nv)]
-    left_gene.extend(right_gene)
-    return add_mution(left_gene)
+    father_gene = [P1.gene[i] for i in range(random_index)]  # set it in a dictionory
+    mother_gene = [P2.gene[i] for i in range(random_index, cnf.nv)]
+    father_gene.extend(mother_gene)
+    return add_mution(father_gene)
 
 first_cnf_example = [creature(np.random.choice([0, 1], size=cnf.nv)) for i in range(cnf.nv)] #make a first cnf
 parent = []
