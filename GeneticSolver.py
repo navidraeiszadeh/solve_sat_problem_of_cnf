07b887c77973700cba52930af90268635fa048a3
Navid Raeiszadeh <Navidraeiszadeh@gmail.com>
2023-06-26 05:51:15 +0330

    complete genetic

--- GeneticSolver.py
+++ GeneticSolver.py
@@ -8,13 +8,10 @@ from pysat.formula import CNF
 
 cnf = CNF('Input.cnf')
 # number of current generation
-mu = 2 * cnf.nv
+number_of_generation = cnf.nv
 # number of selected parent
-lmda = int(math.sqrt(mu)) #-> 80
-mutation_percent = 0.95
-top_membership_percent = 0.85
-max_iteration = 10 * cnf.clauses_size
-max_answer = cnf.clauses_size
+number_of_selected_parent = 50 
+mutation_percent = 0.8
 
 class creature:
     def __init__(self, gene: list):
@@ -42,5 +39,30 @@ def fitness(cnf_test):
                     count_of_sat += 1
     return count_of_sat                    
             
+def add_mution(cnf_value , num):
+    new_cnf_valuation = cnf_value                # as like as noise
+    variable = random.randint(0, num)     #cnf.nv -> total variable count
+    new_cnf_valuation[variable] = '0' if new_cnf_valuation[variable] == '1' else '1'
+    return new_cnf_valuation
+            
+            
+def choose_parent(generation_list):
+    global number_of_generation
+    best_member = int(number_of_selected_parent * 0.8)
+    bottom_gen_num = number_of_selected_parent - best_member
+    best_parent = [generation_list[i] for i in range((number_of_generation-best_member), number_of_generation)]
+    parent_list2 = [generation_list[i] for i in range(0, bottom_gen_num)]
+    return parent_list2.extend(best_parent)
+            
 
 
+def cross_over(P1 , P2):
+    global cnf
+    random_index = random.randint( 1 , cnf.nv)
+    left_gene = [P1.gene[i] for i in range(random_index)]  # set it in a dictionory
+    right_gene = [P2.gene[i] for i in range(random_index, cnf.nv)]
+    left_gene.extend(right_gene)
+    return add_mution(left_gene)
+
+first_cnf_example = [creature(np.random.choice([0, 1], size=cnf.nv)) for i in range(cnf.nv)] #make a first cnf
+parent = []
